a060e0a5ecd0f842f98dc808a167777e71056d32
FaiyazKhalid <fkhalid42@gmail.com>
2024-09-18 04:46:31 +0530

    designed front card view

--- app/src/main/java/com/example/wikisearch/ui/MainActivity.kt
+++ app/src/main/java/com/example/wikisearch/ui/MainActivity.kt
@@ -2,16 +2,22 @@ package com.example.wikisearch.ui
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.drawable.Icon
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -21,15 +27,17 @@ import androidx.navigation.ui.NavigationUI.navigateUp
 import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.util.TableInfo
 import com.example.wikisearch.R
 import com.example.wikisearch.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.logging.Handler
+
 
 
 @AndroidEntryPoint
@@ -38,7 +46,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     companion object{
-         lateinit var viewState: MainViewState
+        lateinit var viewState: MainViewState
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,9 +68,7 @@ class MainActivity : AppCompatActivity() {
         viewState = MainViewState()
         binding.viewState = viewState
 
-        recyclerView = findViewById(R.id.recyclerView)
-
-
+        /*recyclerView = findViewById(R.id.recyclerView)
         val retrofitBuilder = Retrofit.Builder()
             .baseUrl("https://advocatepedia.com/feed/")
             .addConverterFactory(GsonConverterFactory.create())
@@ -86,9 +92,9 @@ class MainActivity : AppCompatActivity() {
             override fun onFailure(call: Call<MyData?>, t: Throwable) {
                 Log.d("Main Activity", "onFailure:" + t.message)
             }
-        })
+        })*/
 
-        val textView = findViewById<TextView>(R.id.textView)
+        val cardView1 = findViewById<CardView>(R.id.wish)
 
         val retrofitBuilder1 = Retrofit.Builder()
             .baseUrl("https://advocatepedia.com/feed/")
@@ -99,6 +105,68 @@ class MainActivity : AppCompatActivity() {
         val retrofitData1 = retrofitBuilder1.getProductData()
         retrofitData1.enqueue(object : Callback<MyData?> {
 
+
+            @RequiresApi(Build.VERSION_CODES.M)
+            @SuppressLint("WrongViewCast")
+            override fun onResponse(call: Call<MyData?>, response: Response<MyData?>) {
+                val responseBody = response.body()
+                val productList = responseBody?.products
+
+                // ... (rest of your code)
+
+                if (productList != null) {
+                    val productNames = productList.joinToString(separator = "\n") { it.wish }
+                    val productNum = productList.joinToString(separator = "\n") { it.num }
+                    val productText = TextView(this@MainActivity)
+                    productText.text = productNames
+                    productText.textSize = 20f // Set desired text size
+                    val productTextNum = TextView(this@MainActivity)
+                    productTextNum.text = productNum
+                    productTextNum.textSize = 20f // Set desired text size
+                    // Add TextView to CardView (adjust layout as needed)
+                    cardView1.addView(productText)
+
+                    findViewById<Button>(R.id.shareButton).setOnClickListener {
+                        val phoneNumber = productTextNum // Assuming productTextNum is a String containing the phone number
+                        val message = findViewById<TextView>(R.id.messageText).text.toString() // Get the message text
+
+                        val whatsappUrl = "https://api.whatsapp.com/send?phone=$phoneNumber&text=${Uri.encode(message)}"
+                        val intent = Intent(Intent.ACTION_VIEW).apply {
+                            data = Uri.parse(whatsappUrl)
+                        }
+                        startActivity(intent)
+                    }
+
+
+
+
+
+
+
+                } else {
+                    // cardView.text = "No products found"
+                }
+            }
+
+            override fun onFailure(call: Call<MyData?>, t: Throwable) {
+                Log.d("Main Activity", "onFailure: " + t.message)
+                // cardView.text = "Failed to load data"
+            }
+        })
+
+
+        val cardView2 = findViewById<CardView>(R.id.otd)
+
+        val retrofitBuilder2 = Retrofit.Builder()
+            .baseUrl("https://advocatepedia.com/feed/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiInterface::class.java)
+
+        val retrofitData2 = retrofitBuilder2.getProductData()
+        retrofitData2.enqueue(object : Callback<MyData?> {
+
+
             @SuppressLint("WrongViewCast")
             override fun onResponse(call: Call<MyData?>, response: Response<MyData?>) {
                 val responseBody = response.body()
@@ -108,31 +176,134 @@ class MainActivity : AppCompatActivity() {
 
                 if (productList != null) {
                     val productNames = productList.joinToString(separator = "\n") { it.otd }
-                    textView.text = productNames
 
-                    // Add share button
-                    val shareButton = findViewById<Button>(R.id.shareButton) // Replace R.id.shareButton with your actual button ID
+                    val productText = TextView(this@MainActivity)
+                    productText.text = productNames
+                    productText.textSize = 20f // Set desired text size
+
+                    // Add TextView to CardView (adjust layout as needed)
+                    cardView2.addView(productText)
+
+                    val shareButton = findViewById<Button>(R.id.shareButton2) // Replace R.id.shareButton with your actual button ID
                     shareButton.setOnClickListener {
                         val shareIntent = Intent(Intent.ACTION_SEND)
                         shareIntent.type = "text/plain"
                         shareIntent.putExtra(Intent.EXTRA_TEXT, productNames)
                         startActivity(Intent.createChooser(shareIntent, "Share products"))
                     }
-                    // ... (rest of your code)
 
 
                 } else {
-                    textView.text = "No products found"
+                    // cardView.text = "No products found"
                 }
             }
 
             override fun onFailure(call: Call<MyData?>, t: Throwable) {
                 Log.d("Main Activity", "onFailure: " + t.message)
-                textView.text = "Failed to load data"
+                // cardView.text = "Failed to load data"
+            }
+        })
+
+
+        val cardView3 = findViewById<CardView>(R.id.report)
+
+        val retrofitBuilder3 = Retrofit.Builder()
+            .baseUrl("https://advocatepedia.com/feed/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiInterface::class.java)
+
+        val retrofitData3 = retrofitBuilder3.getProductData()
+        retrofitData3.enqueue(object : Callback<MyData?> {
+
+
+            @SuppressLint("WrongViewCast")
+            override fun onResponse(call: Call<MyData?>, response: Response<MyData?>) {
+                val responseBody = response.body()
+                val productList = responseBody?.products
+
+                // ... (rest of your code)
+
+                if (productList != null) {
+                    val productNames = productList.joinToString(separator = "\n") { it.news }
+
+                    val productText = TextView(this@MainActivity)
+                    productText.text = productNames
+                    productText.textSize = 20f // Set desired text size
+
+                    // Add TextView to CardView (adjust layout as needed)
+                    cardView3.addView(productText)
+
+                    val shareButton = findViewById<Button>(R.id.shareButton3) // Replace R.id.shareButton with your actual button ID
+                    shareButton.setOnClickListener {
+                        val shareIntent = Intent(Intent.ACTION_SEND)
+                        shareIntent.type = "text/plain"
+                        shareIntent.putExtra(Intent.EXTRA_TEXT, productNames)
+                        startActivity(Intent.createChooser(shareIntent, "Share products"))
+                    }
+
+                } else {
+                    // cardView.text = "No products found"
+                }
+            }
+
+            override fun onFailure(call: Call<MyData?>, t: Throwable) {
+                Log.d("Main Activity", "onFailure: " + t.message)
+                // cardView.text = "Failed to load data"
+            }
+        })
+
+
+        val cardView4 = findViewById<CardView>(R.id.news)
+
+        val retrofitBuilder4 = Retrofit.Builder()
+            .baseUrl("https://advocatepedia.com/feed/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiInterface::class.java)
+
+        val retrofitData4 = retrofitBuilder4.getProductData()
+        retrofitData4.enqueue(object : Callback<MyData?> {
+
+
+            @SuppressLint("WrongViewCast")
+            override fun onResponse(call: Call<MyData?>, response: Response<MyData?>) {
+                val responseBody = response.body()
+                val productList = responseBody?.products
+
+                // ... (rest of your code)
+
+                if (productList != null) {
+                    val productNames = productList.joinToString(separator = "\n") { it.news }
+
+                    val productText = TextView(this@MainActivity)
+                    productText.text = productNames
+                    productText.textSize = 20f // Set desired text size
+
+                    // Add TextView to CardView (adjust layout as needed)
+                    cardView4.addView(productText)
+
+                    val shareButton = findViewById<Button>(R.id.shareButton4) // Replace R.id.shareButton with your actual button ID
+                    shareButton.setOnClickListener {
+                        val shareIntent = Intent(Intent.ACTION_SEND)
+                        shareIntent.type = "text/plain"
+                        shareIntent.putExtra(Intent.EXTRA_TEXT, productNames)
+                        startActivity(Intent.createChooser(shareIntent, "Share products"))
+                    }
+
+                } else {
+                    // cardView.text = "No products found"
+                }
+            }
+
+            override fun onFailure(call: Call<MyData?>, t: Throwable) {
+                Log.d("Main Activity", "onFailure: " + t.message)
+                // cardView.text = "Failed to load data"
             }
         })
 
     }
+
 
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
